db189d5712e09a67ec16554b063dad26a1110567
Nicklas Vrede <32699906+NicklasVrede@users.noreply.github.com>
2024-05-09 10:28:04 +0200

    Opdaterede kommentarer

--- Decode.py
+++ Decode.py
@@ -7,43 +7,47 @@ from gen_huffman import gen_huffman
 import sys
 
 def decode(kom_fil: str, dekom_fil:str):
-    #åben filen og lav en reader
+    #åbn filen og lav en bitreader
     f = open(kom_fil, "rb")
     reader = BitReader(f)
 
 
-    #bits at læse 32*256 = 8192 bits
+    #bits at læse 32*256. 
     hyppighedstabel = []
     for _ in range(256):
         hyppighedstabel.append(reader.readint32bits())
 
 
-    #generer hoffman træ
+    #generer huffman træ
     rod = gen_huffman(hyppighedstabel)
 
-    #tæl mængden af bytes vi skal læse:
+    #tæl mængden af bytes vi skal læse, så vi kan stoppe:
     n_bytes = sum(hyppighedstabel)
 
 
-    #læs mens vi har "hyppigheder" at læse
+    #åbn filen vi skal skrive til
     with open(dekom_fil, "wb") as f:
+        #læs n_bytes
         for _ in range(n_bytes):
+            #start på roden.
             current_node = rod
+            #Gå indtil vi rammer et blad med en byteværdi der ikke er -1.
             while current_node.byteværdi == -1:
-                bit = reader.readbit()
+                bit = reader.readbit() #Vi læser videre fra efter hyppighederne.
+                #retning afhænger af bitværdi
                 if bit == 0:
                     current_node = current_node.venstre
                 else:
                     current_node = current_node.højre
             
-            #skriv byten til filen
+            #Omkonventer byteværdien til en byte og skriv den.
             f.write(bytes([current_node.byteværdi]))
 
     reader.close() #luk filen
 
 
 if __name__ == "__main__":
-    #sys.argv = ["", "testEncoded.txt", "testDecoded.txt"]
+    #sys.argv = ["", "testEncoded.txt", "testDecoded.txt"] #For at teste
 
     kom_fil = sys.argv[1]
     dekom_fil = sys.argv[2]
